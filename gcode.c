3bdc3b479391d5ef6b4edcb93ff1a147b1fc66c7
pylover <vahid.mardani@gmail.com>
2020-03-31 04:40:22 +0430

    Repeate events using epoll timerfd

--- gcode.c
+++ gcode.c
@@ -1,24 +1,24 @@
 #include "common.h"
 #include "gcode.h"
 
-#define STEP "1"
+#define STEP 1
 
 
 int gcodeget(struct js_event *e, char *outbuff, int *outlen) {
+    char axis;
+    int step;
     if (e->type == JS_EVENT_AXIS) {
-        if (e->number == JS_AXIS_X) {
-            if (e->value > 0) {
-                *outlen = sprintf(outbuff, "G0X" STEP);
-                return GCODE_REPEAT;
-            }
-            else if (e->value < 0) {
-                *outlen = sprintf(outbuff, "G0X-" STEP);
-                return GCODE_REPEAT;
-            }
-            else {
-                return OK;
-            }
+        if (e->value == 0) {
+            return OK; // stop repeate
         }
+
+        axis = e->number == JS_AXIS_X? 'X': 'Y';
+        step = e->value > 0? -STEP: STEP;
+        if (axis == 'Y') {
+            step = -step;
+        }
+        *outlen = sprintf(outbuff, "G0%c%d", axis, step);
+        return GCODE_REPEAT;
     }
     return ERR;;
 }
