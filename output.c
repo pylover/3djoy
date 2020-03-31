129d7931005d0202174b3304c71f44669776b308
pylover <vahid.mardani@gmail.com>
2020-04-01 01:20:11 +0430

    Cleanup:

--- output.c
+++ output.c
@@ -39,13 +39,12 @@ static int tcpopen() {
 	serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr = *((struct in_addr *)he->h_addr);
 	serv_addr.sin_port = htons(settings.tcpport); 
-//    err = inet_pton(AF_INET, settings.output, &serv_addr.sin_addr);
-//	if(err == ERR) {
-//		errno = EFAULT;
-//        return ERR;
-//	}
 
-	infoln("Connecting to %s", inet_ntoa(serv_addr.sin_addr));
+	infoln(
+        "Connecting to %s:%d", 
+        inet_ntoa(serv_addr.sin_addr), 
+        settings.tcpport
+    );
 	err = connect(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 	if (err == ERR) {
         return err;
